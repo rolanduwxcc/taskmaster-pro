ff2b0ce325646884b16026fcad851c1e1f33f486
rolanduwxcc <rolanduwxcc@gmail.com>
2021-01-14 00:51:40 -0600

    delete works and touch works, added touch punch

--- assets/js/script.js
+++ assets/js/script.js
@@ -223,6 +223,22 @@ $(".card .list-group").sortable({
   }
 });
 
+//--------------------------------------------------------TRASH
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop",ui);
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
+
 
 // remove all tasks
 $("#remove-tasks").on("click", function() {
