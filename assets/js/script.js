f1bc2f0688dd9c52f94b8dd8145789261aa8334f
rolanduwxcc <rolanduwxcc@gmail.com>
2021-01-14 00:19:26 -0600

    sortable implemented

--- assets/js/script.js
+++ assets/js/script.js
@@ -172,6 +172,56 @@ $(".list-group").on("blur", "input[type='text']", function() {
   $(this).replaceWith(taskSpan);
 });
 
+//--------------------------------------------------------sortable
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  help: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    var tempArray = []; 
+    // console.log("update", this);
+    // console.log($(this).children());
+    $(this).children().each(function() {
+
+      var text = $(this)
+      .find("p")
+      .text()
+      .trim();
+
+      var date = $(this)
+      .find("span")
+      .text()
+      .trim();
+
+      tempArray.push({
+        text: text,
+        date: date
+      });
+    });
+    //trim down list's ID to match object property
+    var arrayName = $(this)
+    .attr("id")
+    .replace("list-", "");
+
+    //update array on tasks object and save
+    tasks[arrayName] = tempArray;
+    saveTasks();
+
+  }
+});
 
 
 // remove all tasks
