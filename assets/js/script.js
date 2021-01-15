47071599939a174ee2e7ae468d78f9090b4ff8ec
rolanduwxcc <rolanduwxcc@gmail.com>
2021-01-15 02:03:56 -0600

    color changes for over due or nearing due tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  //check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -156,7 +158,7 @@ $(".list-group").on("click", "span", function() {
   dateInput.trigger("focus");
 });
 
-//--------------------------------------------------------blur callback left date field return to normal
+//--------------------------------------------------------date CHANGE fka blur callback left date field return to normal
 $(".list-group").on("change", "input[type='text']", function() {
   //get current text
   var taskDate = $(this).val();
@@ -181,6 +183,9 @@ $(".list-group").on("change", "input[type='text']", function() {
 
   //replace input with span element
   $(this).replaceWith(taskSpan);
+
+  //pass the task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 //--------------------------------------------------------sortable
@@ -269,6 +274,26 @@ $("#remove-tasks").on("click", function() {
   }
   saveTasks();
 });
+
+var auditTask = function(taskEl) {
+  //get date from task element
+  var date = $(taskEl).find("span").text().trim();
+
+  //convert to moment object at 5pm
+  var time = moment(date, "L").set("hour", 17);
+  //this should print out an object for the value of the date variable but at 5pm (military time is 1700 hours).
+
+  //remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  //apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
 
 // load tasks for the first time
 loadTasks();
