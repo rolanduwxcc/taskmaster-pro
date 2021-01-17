a4009b2ebdcb8bee1fef9c5a327f0f74d18029f3
rolanduwxcc <rolanduwxcc@gmail.com>
2021-01-17 16:54:49 -0600

    added custom CSS and icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,7 +60,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -196,15 +196,26 @@ $(".card .list-group").sortable({
   help: "clone",
   activate: function(event) {
     console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
     console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
     console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event) {
     console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
+    $(".bottom-trash").removeClass("bottom-trash-active");
+  },
+  drop: function(event) {
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   update: function(event) {
     var tempArray = []; 
@@ -276,6 +287,7 @@ $("#remove-tasks").on("click", function() {
 });
 
 var auditTask = function(taskEl) {
+  // console.log(taskEl);
   //get date from task element
   var date = $(taskEl).find("span").text().trim();
 
@@ -294,6 +306,14 @@ var auditTask = function(taskEl) {
     $(taskEl).addClass("list-group-item-warning");
   }
 };
+
+//this thing right here is good example of how traverse the DOM and get the element you want
+//to mess with 
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
 
 // load tasks for the first time
 loadTasks();
